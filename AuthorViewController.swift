cc86ddf657fe4ee1e131cfede697278073fba79a
Herve Desrosiers <herve_desrosiers@hotmail.com>
2019-11-17 09:20:47 -0500

    added author scene

---
+++ AuthorViewController.swift
@@ -0,0 +1,20 @@
+//
+//  AuthorViewController.swift
+//  BullseyeKit
+//
+//  Created by Herve Desrosiers on 2019-11-17.
+//  Copyright © 2019 Herve Desrosiers. All rights reserved.
+//
+
+import UIKit
+
+class AuthorViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+    @IBAction func close() {
+        dismiss(animated: true, completion: nil)
+    }
+}
